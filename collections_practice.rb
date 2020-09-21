cec5a6b49cb6bcd0971113f3688c554f6c6e0723
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 01:28:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  
+  array.sort
+  
+end
 
+def sort_array_desc(arrays)
+  
+  arrays.sort! {|x,y| y <=> x }
+end
+
+def sort_array_char_count
+
+def swap_elements (array2,index, destination_index)
+  
+  array2.sort {|a,b| }
+  
+  
+end 
+
+def reverse_array (new)
+  new.reverse { |copy| puts copy.join}
+
+end
+  
+def kesha_maker(kesh)
+  kesh.each{ |dollar| dollar.sort_by("$")}
+end
+  
+  
+  
+  
